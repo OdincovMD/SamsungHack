f316855f7bfa3d71ce21c7b93cbafa80842bfe24
PO <p.orlova18@mail.ru>
2023-01-27 01:41:59 +0300

    Добавлен permission для камеры

--- app/src/main/java/com/dmiiy/wayapp/ScanqrActivity.kt
+++ app/src/main/java/com/dmiiy/wayapp/ScanqrActivity.kt
@@ -1,9 +1,13 @@
 package com.dmiiy.wayapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import me.dm7.barcodescanner.zbar.Result
 import me.dm7.barcodescanner.zbar.ZBarScannerView
 
@@ -13,28 +17,26 @@ class ScanqrActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
         super.onCreate(savedInstanceState)
         zbView = ZBarScannerView(this)
         setContentView(zbView)
-        val requestPermissionLauncher =
-            registerForActivityResult(
-                ActivityResultContracts.RequestPermission()
-            ) { isGranted: Boolean ->
-                if (isGranted) {
-                    // Permission is granted. Continue the action or workflow in your
-                    // app.
-                } else {
-                    // Explain to the user that the feature is unavailable because the
-                    // feature requires a permission that the user has denied. At the
-                    // same time, respect the user's decision. Don't link to system
-                    // settings in an effort to convince the user to change their
-                    // decision.
-                }
-            }
     }
 
     override fun onResume() {
         super.onResume()
         zbView.setResultHandler(this)
+        checkCameraPermission()
         zbView.startCamera()
     }
+
+    //Разрешение на использование камеры
+    private fun checkCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA), 1)
+
+        }
+    }
+
 
     override fun onPause() {
         super.onPause()
